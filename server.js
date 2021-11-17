c8aa9cc88bd7bee673f3c511276c3d7926f20993
genadireuben <genadireuben91@gmail.com>
2021-11-17 20:48:18 -0300

    env

--- server.js
+++ server.js
@@ -2,19 +2,14 @@ require('dotenv').config();
 // const path = require('path');
 const express = require('express');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 const http = require('http').createServer(app);
 
-const io = require('socket.io')(http, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
-});
+const io = require('socket.io')(http, PORT);
 const chatController = require('./controllers/chatController');
 const ioChat = require('./socket/messages');
 
-const PORT = process.env.PORT || 3000;
 
 ioChat(io);
 
